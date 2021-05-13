53ce6fc5135d41094b70e7337ba35bf5af0e1f95
mixree <75112956+hurrytears@users.noreply.github.com>
2021-05-13 17:40:04 +0800

    maybe changed

--- examples/src/main/java/org/apache/spark/examples/JavaWordCount.java
+++ examples/src/main/java/org/apache/spark/examples/JavaWordCount.java
@@ -28,32 +28,33 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public final class JavaWordCount {
-  private static final Pattern SPACE = Pattern.compile(" ");
+    private static final Pattern SPACE = Pattern.compile(" ");
 
-  public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
+        args = new String[]{"mytest/words"};
 
-    if (args.length < 1) {
-      System.err.println("Usage: JavaWordCount <file>");
-      System.exit(1);
+        if (args.length < 1) {
+            System.err.println("Usage: JavaWordCount <file>");
+            System.exit(1);
+        }
+
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("JavaWordCount")
+                .getOrCreate();
+
+        JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
+
+        JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
+
+        JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
+
+        JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
+
+        List<Tuple2<String, Integer>> output = counts.collect();
+        for (Tuple2<?, ?> tuple : output) {
+            System.out.println(tuple._1() + ": " + tuple._2());
+        }
+        spark.stop();
     }
-
-    SparkSession spark = SparkSession
-      .builder()
-      .appName("JavaWordCount")
-      .getOrCreate();
-
-    JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
-
-    JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
-
-    JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
-
-    JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
-
-    List<Tuple2<String, Integer>> output = counts.collect();
-    for (Tuple2<?,?> tuple : output) {
-      System.out.println(tuple._1() + ": " + tuple._2());
-    }
-    spark.stop();
-  }
 }
