4c20ed4507fbdd525cf71e5ec0840b74f3c75bb3
sosog <hurrytears@gmail.com>
2021-08-31 23:01:35 +0800

    streaming sleep2

--- common/network-common/src/main/java/org/apache/spark/network/server/TransportServer.java
+++ common/network-common/src/main/java/org/apache/spark/network/server/TransportServer.java
@@ -44,136 +44,138 @@ import org.apache.spark.network.util.*;
  * Server for the efficient, low-level streaming service.
  */
 public class TransportServer implements Closeable {
-  private static final Logger logger = LoggerFactory.getLogger(TransportServer.class);
+    private static final Logger logger = LoggerFactory.getLogger(TransportServer.class);
 
-  private final TransportContext context;
-  private final TransportConf conf;
-  private final RpcHandler appRpcHandler;
-  private final List<TransportServerBootstrap> bootstraps;
+    private final TransportContext context;
+    private final TransportConf conf;
+    private final RpcHandler appRpcHandler;
+    private final List<TransportServerBootstrap> bootstraps;
 
-  private ServerBootstrap bootstrap;
-  private ChannelFuture channelFuture;
-  private int port = -1;
-  private final PooledByteBufAllocator pooledAllocator;
-  private NettyMemoryMetrics metrics;
+    private ServerBootstrap bootstrap;
+    private ChannelFuture channelFuture;
+    private int port = -1;
+    private final PooledByteBufAllocator pooledAllocator;
+    private NettyMemoryMetrics metrics;
 
-  /**
-   * Creates a TransportServer that binds to the given host and the given port, or to any available
-   * if 0. If you don't want to bind to any special host, set "hostToBind" to null.
-   * */
-  public TransportServer(
-      TransportContext context,
-      String hostToBind,
-      int portToBind,
-      RpcHandler appRpcHandler,
-      List<TransportServerBootstrap> bootstraps) {
-    this.context = context;
-    this.conf = context.getConf();
-    this.appRpcHandler = appRpcHandler;
-    if (conf.sharedByteBufAllocators()) {
-      this.pooledAllocator = NettyUtils.getSharedPooledByteBufAllocator(
-          conf.preferDirectBufsForSharedByteBufAllocators(), true /* allowCache */);
-    } else {
-      this.pooledAllocator = NettyUtils.createPooledByteBufAllocator(
-          conf.preferDirectBufs(), true /* allowCache */, conf.serverThreads());
-    }
-    this.bootstraps = Lists.newArrayList(Preconditions.checkNotNull(bootstraps));
-
-    boolean shouldClose = true;
-    try {
-      init(hostToBind, portToBind);
-      shouldClose = false;
-    } finally {
-      if (shouldClose) {
-        JavaUtils.closeQuietly(this);
-      }
-    }
-  }
-
-  public int getPort() {
-    if (port == -1) {
-      throw new IllegalStateException("Server not initialized");
-    }
-    return port;
-  }
-
-  private void init(String hostToBind, int portToBind) {
-
-    IOMode ioMode = IOMode.valueOf(conf.ioMode());
-    EventLoopGroup bossGroup = NettyUtils.createEventLoop(ioMode, 1,
-      conf.getModuleName() + "-boss");
-    EventLoopGroup workerGroup =  NettyUtils.createEventLoop(ioMode, conf.serverThreads(),
-      conf.getModuleName() + "-server");
-
-    bootstrap = new ServerBootstrap()
-      .group(bossGroup, workerGroup)
-      .channel(NettyUtils.getServerChannelClass(ioMode))
-      .option(ChannelOption.ALLOCATOR, pooledAllocator)
-      .option(ChannelOption.SO_REUSEADDR, !SystemUtils.IS_OS_WINDOWS)
-      .childOption(ChannelOption.ALLOCATOR, pooledAllocator);
-
-    this.metrics = new NettyMemoryMetrics(
-      pooledAllocator, conf.getModuleName() + "-server", conf);
-
-    if (conf.backLog() > 0) {
-      bootstrap.option(ChannelOption.SO_BACKLOG, conf.backLog());
-    }
-
-    if (conf.receiveBuf() > 0) {
-      bootstrap.childOption(ChannelOption.SO_RCVBUF, conf.receiveBuf());
-    }
-
-    if (conf.sendBuf() > 0) {
-      bootstrap.childOption(ChannelOption.SO_SNDBUF, conf.sendBuf());
-    }
-
-    if (conf.enableTcpKeepAlive()) {
-      bootstrap.childOption(ChannelOption.SO_KEEPALIVE, true);
-    }
-
-    bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
-      @Override
-      protected void initChannel(SocketChannel ch) {
-        logger.debug("New connection accepted for remote address {}.", ch.remoteAddress());
-
-        RpcHandler rpcHandler = appRpcHandler;
-        for (TransportServerBootstrap bootstrap : bootstraps) {
-          rpcHandler = bootstrap.doBootstrap(ch, rpcHandler);
+    /**
+     * Creates a TransportServer that binds to the given host and the given port, or to any available
+     * if 0. If you don't want to bind to any special host, set "hostToBind" to null.
+     */
+    public TransportServer(
+            TransportContext context,
+            String hostToBind,
+            int portToBind,
+            RpcHandler appRpcHandler,
+            List<TransportServerBootstrap> bootstraps) {
+        // abc
+        this.context = context;
+        this.conf = context.getConf();
+        this.appRpcHandler = appRpcHandler;
+        if (conf.sharedByteBufAllocators()) {
+            this.pooledAllocator = NettyUtils.getSharedPooledByteBufAllocator(
+                    conf.preferDirectBufsForSharedByteBufAllocators(), true /* allowCache */);
+        } else {
+            this.pooledAllocator = NettyUtils.createPooledByteBufAllocator(
+                    conf.preferDirectBufs(), true /* allowCache */, conf.serverThreads());
         }
-        context.initializePipeline(ch, rpcHandler);
-      }
-    });
+        this.bootstraps = Lists.newArrayList(Preconditions.checkNotNull(bootstraps));
 
-    InetSocketAddress address = hostToBind == null ?
-        new InetSocketAddress(portToBind): new InetSocketAddress(hostToBind, portToBind);
-    channelFuture = bootstrap.bind(address);
-    channelFuture.syncUninterruptibly();
-
-    port = ((InetSocketAddress) channelFuture.channel().localAddress()).getPort();
-    logger.debug("Shuffle server started on port: {}", port);
-  }
-
-  public MetricSet getAllMetrics() {
-    return metrics;
-  }
-
-  @Override
-  public void close() {
-    if (channelFuture != null) {
-      // close is a local operation and should finish within milliseconds; timeout just to be safe
-      channelFuture.channel().close().awaitUninterruptibly(10, TimeUnit.SECONDS);
-      channelFuture = null;
+        boolean shouldClose = true;
+        try {
+            init(hostToBind, portToBind);
+            shouldClose = false;
+        } finally {
+            if (shouldClose) {
+                JavaUtils.closeQuietly(this);
+            }
+        }
     }
-    if (bootstrap != null && bootstrap.config().group() != null) {
-      bootstrap.config().group().shutdownGracefully();
-    }
-    if (bootstrap != null && bootstrap.config().childGroup() != null) {
-      bootstrap.config().childGroup().shutdownGracefully();
-    }
-    bootstrap = null;
-  }
 
-  public Counter getRegisteredConnections() {
-    return context.getRegisteredConnections();
-  }
+    public int getPort() {
+        if (port == -1) {
+            throw new IllegalStateException("Server not initialized");
+        }
+        return port;
+    }
+
+    private void init(String hostToBind, int portToBind) {
+
+        IOMode ioMode = IOMode.valueOf(conf.ioMode());
+        EventLoopGroup bossGroup = NettyUtils.createEventLoop(ioMode, 1,
+                conf.getModuleName() + "-boss");
+        EventLoopGroup workerGroup = NettyUtils.createEventLoop(ioMode, conf.serverThreads(),
+                conf.getModuleName() + "-server");
+
+        // 找到啦，netty的经典套路
+        bootstrap = new ServerBootstrap()
+                .group(bossGroup, workerGroup)
+                .channel(NettyUtils.getServerChannelClass(ioMode))
+                .option(ChannelOption.ALLOCATOR, pooledAllocator)
+                .option(ChannelOption.SO_REUSEADDR, !SystemUtils.IS_OS_WINDOWS)
+                .childOption(ChannelOption.ALLOCATOR, pooledAllocator);
+
+        this.metrics = new NettyMemoryMetrics(
+                pooledAllocator, conf.getModuleName() + "-server", conf);
+
+        if (conf.backLog() > 0) {
+            bootstrap.option(ChannelOption.SO_BACKLOG, conf.backLog());
+        }
+
+        if (conf.receiveBuf() > 0) {
+            bootstrap.childOption(ChannelOption.SO_RCVBUF, conf.receiveBuf());
+        }
+
+        if (conf.sendBuf() > 0) {
+            bootstrap.childOption(ChannelOption.SO_SNDBUF, conf.sendBuf());
+        }
+
+        if (conf.enableTcpKeepAlive()) {
+            bootstrap.childOption(ChannelOption.SO_KEEPALIVE, true);
+        }
+
+        bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel ch) {
+                logger.debug("New connection accepted for remote address {}.", ch.remoteAddress());
+
+                RpcHandler rpcHandler = appRpcHandler;
+                for (TransportServerBootstrap bootstrap : bootstraps) {
+                    rpcHandler = bootstrap.doBootstrap(ch, rpcHandler);
+                }
+                context.initializePipeline(ch, rpcHandler);
+            }
+        });
+
+        InetSocketAddress address = hostToBind == null ?
+                new InetSocketAddress(portToBind) : new InetSocketAddress(hostToBind, portToBind);
+        channelFuture = bootstrap.bind(address);
+        channelFuture.syncUninterruptibly();
+
+        port = ((InetSocketAddress) channelFuture.channel().localAddress()).getPort();
+        logger.debug("Shuffle server started on port: {}", port);
+    }
+
+    public MetricSet getAllMetrics() {
+        return metrics;
+    }
+
+    @Override
+    public void close() {
+        if (channelFuture != null) {
+            // close is a local operation and should finish within milliseconds; timeout just to be safe
+            channelFuture.channel().close().awaitUninterruptibly(10, TimeUnit.SECONDS);
+            channelFuture = null;
+        }
+        if (bootstrap != null && bootstrap.config().group() != null) {
+            bootstrap.config().group().shutdownGracefully();
+        }
+        if (bootstrap != null && bootstrap.config().childGroup() != null) {
+            bootstrap.config().childGroup().shutdownGracefully();
+        }
+        bootstrap = null;
+    }
+
+    public Counter getRegisteredConnections() {
+        return context.getRegisteredConnections();
+    }
 }
